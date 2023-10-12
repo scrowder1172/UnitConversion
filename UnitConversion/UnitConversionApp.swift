295065121866a85e5ee7fb948e827833785f0c06
Scott Crowder <scrowder@gmail.com>
2023-10-12 14:51:23 -0500

    Initial Commit

---
+++ UnitConversion/UnitConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionApp.swift
+//  UnitConversion
+//
+//  Created by SCOTT CROWDER on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
